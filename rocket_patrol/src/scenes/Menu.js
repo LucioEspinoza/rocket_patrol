d14a589231688bb6080562a338df18d296234d82
Lucio Espinoza <lufespin@ucsc.edu>
2020-04-08 15:47:13 -0700

    added scenes and assests

---
+++ rocket_patrol/src/scenes/Menu.js
@@ -0,0 +1,14 @@
+class Menu extends Phaser.Scene{
+    constructor(){
+        super("menuScene");
+    }
+
+    create(){
+        console.log(this);
+        //Display menu text
+        this.add.text(20, 20, "Rocket Patrol Menu");
+    
+        //Launches the next scene
+        this.scene.start("playScene");
+    }
+}
